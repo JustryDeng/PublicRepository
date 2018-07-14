ce9c798883a6dbd8f17ff317e25f1e136b19b066
JustryDeng <m13548417409@163.com>
2018-07-14 11:50:25 +0800

    HttpClient详细使用示例

---
+++ Abc_HttpClientDemo/src/main/java/com/AbcHttpClientDemoApplication.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcHttpClientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcHttpClientDemoApplication.class, args);
+	}
+}
