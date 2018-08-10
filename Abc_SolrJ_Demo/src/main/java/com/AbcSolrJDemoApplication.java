54467787d6c1d147cb97842efee30ab384c254fd
JustryDeng <m13548417409@163.com>
2018-08-10 15:37:33 +0800

    SpringBoot使用SolrJ(注:SpringBoot使用SolrJ示例，与[Sor安装搭建_Solr图形面板介绍_普通java项目使用Solr示例]中的普通项目几乎一样，只是改了很少一点地方，见博客)

---
+++ Abc_SolrJ_Demo/src/main/java/com/AbcSolrJDemoApplication.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcSolrJDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcSolrJDemoApplication.class, args);
+	}
+}
