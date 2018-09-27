074d9499f4e3c74442869f4a2c414beebe614613
unknown <m13548417409@163.com>
2018-09-27 15:14:07 +0800

    java实现FTP上传(文件)、下载(文件、文件夹、文件夹递归)、删除(文件、文件夹递归)

---
+++ Abc_FTP_UploadAndDownload/src/test/java/com/AbcFtpUploadAndDownloadApplicationTests.java
@@ -0,0 +1,110 @@
+package com;
+
+import java.io.IOException;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.aspire.util.FTPUtils;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AbcFtpUploadAndDownloadApplicationTests {
+
+//	/**
+//	 * deleteBlankDirOrFile删除 --- 测试
+//	 * 
+//	 * @DATE 2018年9月27日 上午10:12:56
+//	 */
+//	public static void main(String[] args) throws IOException {
+//		/// 基本配置
+//		FTPUtils ftpUtils = FTPUtils.getFTPUtilsInstance("10.2.6.16", 22, "用户名", "密码");
+//		// 注:根据不同的(Server/Client)情况,这里灵活设置
+//		//    编码不对可能导致文件删除失败
+//		ftpUtils.setSendCommandStringEncoding("GBK");
+//		
+//		String remoteDir = new String("/test/JustryDeng.shuaige");
+//		// 执行删除
+//		ftpUtils.deleteBlankDirOrFile(remoteDir);
+//	}
+	
+//	/**
+//	 * recursiveDeleteBlankDirOrFile(即:deleteBlankDirOrFile升级版)删除 --- 测试
+//	 * 
+//	 * @DATE 2018年9月26日 下午4:51:51
+//	 */
+//	public static void main(String[] args) throws IOException {
+//		/// 基本配置
+//		FTPUtils ftpUtils = FTPUtils.getFTPUtilsInstance("10.2.6.16", 22, "用户名", "密码");
+//		// 注:根据不同的(Server/Client)情况,这里灵活设置
+//		//    编码不对可能导致文件删除失败
+//		ftpUtils.setSendCommandStringEncoding("GBK");
+//		
+//		String remoteDir = new String("/test");
+//		// 执行删除
+//		ftpUtils.recursiveDeleteBlankDirOrFile(remoteDir);
+//	}
+	
+	/**
+	 * downloadFile下载 --- 测试
+	 * 
+	 * @DATE 2018年9月26日 下午4:51:51
+	 */
+	public static void main(String[] args) throws IOException {
+		/// 基本配置
+		FTPUtils ftpUtils = FTPUtils.getFTPUtilsInstance("10.2.6.16", 22, "用户名", "密码");
+		// 注:根据不同的(Server/Client)情况,这里灵活设置
+		ftpUtils.setSendCommandStringEncoding("ISO-8859-1");
+		// 注:根据不同的(Server/Client)情况,这里灵活设置
+		ftpUtils.setDownfileNameEncodingParam1("ISO-8859-1");
+		ftpUtils.setDownfileNameDecodingParam2("GBK");
+		
+		String remoteDir = new String("/test/we.asd");
+		String localDir = new String("C:/Users/JustryDeng/Desktop/木头人12345");
+		// 下载
+		ftpUtils.downloadFile(remoteDir,localDir);
+	}
+	
+//	/**
+//	 * recursiveDownloadFile(即:downloadFile升级版)下载 --- 测试
+//	 * 
+//	 * @DATE 2018年9月26日 下午4:51:51
+//	 */
+//	public static void main(String[] args) throws IOException {
+//		/// 基本配置
+//		FTPUtils ftpUtils = FTPUtils.getFTPUtilsInstance("10.2.6.16", 22, "用户名", "密码");
+//		// 注:根据不同的(Server/Client)情况,这里灵活设置
+//		ftpUtils.setSendCommandStringEncoding("ISO-8859-1");
+//		// 注:根据不同的(Server/Client)情况,这里灵活设置
+//		ftpUtils.setDownfileNameEncodingParam1("ISO-8859-1");
+//		ftpUtils.setDownfileNameDecodingParam2("GBK");
+//		
+//		String remoteDir = new String("/test");
+//		String localDir = new String("C:/Users/JustryDeng/Desktop/Justry帅");
+//		// 执行下载
+//		ftpUtils.recursiveDownloadFile(remoteDir, localDir);
+//	}
+
+	
+//	/**
+//	 * uploadFile上传 --- 测试
+//	 * 
+//	 * @DATE 2018年9月26日 下午4:51:51
+//	 */
+//	public static void main(String[] args) throws IOException {
+//		/// 基本配置
+//		FTPUtils ftpUtils = FTPUtils.getFTPUtilsInstance("10.2.6.16", 22, "用户名", "密码");
+//		// 上传文件中有中文,这里设置为GBK
+//		// 注:根据不同的(Server/Client)情况,这里灵活设置
+//		ftpUtils.setSendCommandStringEncoding("GBK");
+//		// 注:根据要上传下载的文件的情况,这里灵活设置
+//		ftpUtils.setTransportFileType(FTP.BINARY_FILE_TYPE);
+//		
+//		String s = new String("C:/Users/JustryDeng/Desktop/SQL语句.txt");
+//		File file = new File(s);
+//		//执行上传
+//		ftpUtils.uploadFile("/test", "justry_deng.html", file);
+//	}
+
+}
